4157522c17ae4b08c3f271f890a7dda4d79c65e4
Dima Portenko <troublesohard@gmail.com>
2023-09-03 01:05:24 +0300

    fix nvim-tree config

--- nvim/lua/dimaportenko/nvim-tree.lua
+++ nvim/lua/dimaportenko/nvim-tree.lua
@@ -2,15 +2,9 @@
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
+  error "Cannot load nvim-tree"
   return
 end
-
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
 
 local function my_on_attach(bufnr)
   local api = require('nvim-tree.api')
