085ad2590f3f2329e7e76421c779f3f0807bd056
Dima Portenko <troublesohard@gmail.com>
2024-04-10 15:53:52 +0300

    telescope config update

--- nvim/lua/dimaportenko/telescope.lua
+++ nvim/lua/dimaportenko/telescope.lua
@@ -46,6 +46,10 @@ telescope.setup {
 
         ["<C-u>"] = actions.preview_scrolling_up,
         ["<C-d>"] = actions.preview_scrolling_down,
+        ["<C-h>"] = actions.preview_scrolling_left,
+        ["<C-l>"] = actions.preview_scrolling_right,
+
+        ['<C-f>'] = require('telescope.actions.layout').toggle_preview,
 
         ["<PageUp>"] = actions.results_scrolling_up,
         ["<PageDown>"] = actions.results_scrolling_down,
@@ -54,7 +58,7 @@ telescope.setup {
         ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
         ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-l>"] = actions.complete_tag,
+        -- ["<C-l>"] = actions.complete_tag,
         ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
       },
 
@@ -83,6 +87,8 @@ telescope.setup {
 
         ["<C-u>"] = actions.preview_scrolling_up,
         ["<C-d>"] = actions.preview_scrolling_down,
+        ["<C-h>"] = actions.preview_scrolling_left,
+        ["<C-l>"] = actions.preview_scrolling_right,
 
         ["<PageUp>"] = actions.results_scrolling_up,
         ["<PageDown>"] = actions.results_scrolling_down,
@@ -105,7 +111,7 @@ telescope.setup {
       -- filetypes whitelist
       -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
       filetypes = { "png", "webp", "jpg", "jpeg" },
-      find_cmd = "rg"   -- find command (defaults to `fd`)
+      find_cmd = "rg" -- find command (defaults to `fd`)
     }
     -- Your extension configuration goes here:
     -- extension_name = {
